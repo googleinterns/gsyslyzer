791aa348088cd9509e3de4eb15e95d3926177c77
rngomez303 <rngomez@google.com>
2020-07-24 10:43:17 -0600

    Initial check-in of source code for parsing log events into event groups.

---
+++ src/event_parser.py
@@ -0,0 +1,86 @@
+from datetime import datetime
+from event_group_rule import EventGroupRule
+from log_event_group import LogEventGroup
+from log_parser import LogEvent, LogParser
+
+""" Module for parsing Log Events into Event Groups """
+
+class LogEventParser:
+    """ Parser for applying group rules against a list of events to
+        create instances of event groups
+        
+        Attributes
+        ----------
+        log_events: list of LogEvent objects to be parsed
+        event_group_rules: list of EventGroupRule objects to be applied
+                           for event grouping
+        event_groups_found: chronological list list of detected 
+                            LogEventGroup objects
+        event_groups_found_dict: dictionary of LogEventGroups where
+                                 key is the group tag """
+
+    def __init__(self, log_events, event_group_rules):
+        self.log_events = log_events
+        self.event_group_rules = event_group_rules
+
+        self.event_groups_found = []
+        self.event_groups_found_dict = {}
+
+    def get_event_search_dict(self, event_group_rule):
+        search_dict = {tag: [] for tag in event_group_rule.trigger_event_tags}
+        return search_dict
+
+    def update_search_dict(self, group_tag, search_dict, event, context_event_buffer):
+        ordered_tags = list(search_dict.keys())
+        event_tag = event.tag
+        tag_index = ordered_tags.index(event_tag)
+        next_tag_index = tag_index + 1
+
+        if tag_index == 0:
+            all_log_events = context_event_buffer + [event]
+            
+            event_group = LogEventGroup(tag=group_tag, trigger_log_events=[event],
+                                        context_log_events=context_event_buffer,
+                                        all_log_events=all_log_events)
+
+            context_event_buffer = []
+            
+            next_tag = ordered_tags[next_tag_index]
+            search_dict[next_tag].append(event_group)
+        elif next_tag_index == len(ordered_tags):
+            partial_groups_list = search_dict[event_tag]
+            if len(partial_groups_list) > 0:
+                event_group = partial_groups_list.pop(0)
+                event_group.add_log_event(event)
+            
+                self.event_groups_found.append(event_group)
+
+                if group_tag not in self.event_groups_found_dict:
+                    self.event_groups_found_dict[group_tag] = [event_group]
+                else:
+                    self.event_groups_found_dict[group_tag].append(event_group)
+        else:
+            event_group = search_dict[event_tag].pop(0)
+            event_group.add_log_event(event)
+            
+            next_tag = ordered_tags[next_tag_index] 
+            search_dict[next_tag].append(event_group)
+
+        return context_event_buffer
+
+    def parse_log_events(self):
+        for rule in self.event_group_rules:
+            search_dict = self.get_event_search_dict(rule)
+            context_tags = rule.context_event_tags
+            context_event_buffer = []
+            group_tag = rule.tag
+
+            for event in self.log_events:
+                tag = event.tag
+                if tag in search_dict:
+                    context_event_buffer = self.update_search_dict(group_tag,
+                                                                   search_dict, event, 
+                                                                   context_event_buffer)
+                elif tag in context_tags:
+                    context_event_buffer.append(event)
+
---
+++ src/test_event_parser.py
@@ -0,0 +1,78 @@
+import unittest
+from copy import copy
+import event_parser, constants
+from mocks import MockLogEvent
+""" Module for unit testing the parsing of log events into event groups """
+
+class EventParserTest(unittest.TestCase):
+    """ Test suite for the event parser """
+    def setUp(self):
+        super(EventParserTest, self).setUp()
+
+        # Set up fake event group rules for CAT and DOG groups
+        self.tag_cat = "cat"
+        self.trigger_tags_cat = ["C", "A", "T"]
+        
+        self.tag_dog = "dog"
+        self.trigger_tags_dog = ["D", "O", "G"]
+        self.context_tags_dog = ["X", "Y"]
+
+        event_group_rule_cat = event_parser.EventGroupRule(self.tag_cat, self.trigger_tags_cat)
+        event_group_rule_dog = event_parser.EventGroupRule(self.tag_dog, self.trigger_tags_dog,
+                                              self.context_tags_dog)
+
+        self.event_group_rules = [event_group_rule_cat, event_group_rule_dog]
+
+    def get_test_case_event_parser(self, tags):
+        """ Quick setup for different test cases """
+        log_events = []
+        for tag in tags:
+            mock_log_event = MockLogEvent(tag)
+            log_events.append(mock_log_event)
+
+        log_event_parser = event_parser.LogEventParser(log_events,
+                                                       self.event_group_rules)
+        return log_event_parser
+    
+    def test_coinciding_events(self):
+        test_tags = ["D", "D", "O", "O", "G", "G"]
+        expected_num_groups_found = 2
+
+        parser = self.get_test_case_event_parser(test_tags)
+        parser.parse_log_events()
+        
+        groups_found = parser.event_groups_found
+        num_groups_found = len(groups_found)
+
+        self.assertEqual(num_groups_found, expected_num_groups_found)
+
+    def test_overlapping_context_events(self):
+        test_tags = ["Y", "D", "X", "O", "G", "D", "O", "G"]
+        expected_num_groups_found = 2
+        expected_num_all_events = 4
+
+        parser = self.get_test_case_event_parser(test_tags)
+        parser.parse_log_events()
+
+        groups_found = parser.event_groups_found
+        num_groups_found = len(groups_found)
+        self.assertEqual(num_groups_found, expected_num_groups_found)
+
+        for group in groups_found:
+            num_all_events = len(group.all_log_events)
+            self.assertEqual(num_all_events,expected_num_all_events)
+
+    def test_noisy_events(self):
+        test_tags = ["C", "NOISE", "D", "A", "X", "T", "O", "G", "G"]
+        expected_num_groups_found = 2
+
+        parser = self.get_test_case_event_parser(test_tags)
+        parser.parse_log_events()
+
+        groups_found = parser.event_groups_found
+        num_groups_found = len(groups_found)
+        self.assertEqual(num_groups_found, expected_num_groups_found)
+
+
+if __name__ == "__main__":
+    unittest.main()
