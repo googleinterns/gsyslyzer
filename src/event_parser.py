c1780040b576ca5f2a1e2eff97faadd1abae26cc
rngomez303 <rngomez@google.com>
2020-07-30 15:03:37 -0600

    Reformatted docstrings and comments.

--- src/event_parser.py
+++ src/event_parser.py
@@ -26,7 +26,8 @@ class LogEventParser:
         self.event_groups_found = []
         self.event_groups_found_dict = {}
 
-    """ Initializes a search dictionary for tracking whether a group 
+    def get_event_search_dict(self, event_group_rule):
+        """ Initializes a search dictionary for tracking whether a group 
         has attained all required events, a partial gorup will sit
         in a queue with key of the next event it needs
 
@@ -40,22 +41,23 @@ class LogEventParser:
             
         Uses queues to ensure first occurences of events are grouped
         together """
-    def get_event_search_dict(self, event_group_rule):
+
         search_dict = {tag: [] for tag in event_group_rule.trigger_event_tags}
         return search_dict
 
-    """ Moves partial groups from stage to stage in the search dict,
-        or stores them as valid if they complete """
     def update_search_dict(self, group_tag, search_dict, event, context_event_buffer):
+        """ Moves partial groups from stage to stage in the search dict,
+        or stores them as valid if they complete """
+        
         ordered_tags = list(search_dict.keys())
         event_tag = event.tag
         tag_index = ordered_tags.index(event_tag)
         next_tag_index = tag_index + 1
 
         if tag_index == 0 and next_tag_index == len(ordered_tags):
-            """ In the special case that a group only requires one event
-                the current context buffer and first event found are 
-                immeditaely grouped and stored as a valid group. """
+            # In the special case that a group only requires one event
+            # the current context buffer and first event found are 
+            # immeditaely grouped and stored as a valid group.
             all_log_events = context_event_buffer + [event]
             
             event_group = LogEventGroup(tag=group_tag, trigger_log_events=[event],
@@ -71,9 +73,9 @@ class LogEventParser:
             
             context_event_buffer = []
         elif tag_index == 0:
-            """ In the case that the first event is found, the
-                context buffer and the event found are grouped into a 
-                partial group and stored in the search dictionary. """
+            # In the case that the first event is found, the
+            # context buffer and the event found are grouped into a 
+            # partial group and stored in the search dictionary.
             all_log_events = context_event_buffer + [event]
             
             event_group = LogEventGroup(tag=group_tag, trigger_log_events=[event],
@@ -85,10 +87,11 @@ class LogEventParser:
             next_tag = ordered_tags[next_tag_index]
             search_dict[next_tag].append(event_group)
         elif next_tag_index == len(ordered_tags):
-            """ In the case that the last event is found, the partial
-                group is removed from the search dictionary and 
-                stored as a valid group """
+            # In the case that the last event is found, the partial
+            # group is removed from the search dictionary and 
+            # stored as a valid group.
             partial_groups_list = search_dict[event_tag]
+
             if len(partial_groups_list) > 0:
                 event_group = partial_groups_list.pop(0)
                 event_group.add_log_event(event)
@@ -100,9 +103,9 @@ class LogEventParser:
                 else:
                     self.event_groups_found_dict[group_tag].append(event_group)
         else:
-            """ In the case that it is some other relevant event, the
-                event is added to the partial group at the front of the
-                current queue and then pushed to the next queue """
+            # In the case that it is some other relevant event, the
+            # event is added to the partial group at the front of the
+            # current queue and then pushed to the next queue.
             event_group = search_dict[event_tag].pop(0)
             event_group.add_log_event(event)
             
@@ -111,11 +114,12 @@ class LogEventParser:
 
         return context_event_buffer
 
-    """ Driving method for parsing the log events. Each rule is applied
+    def parse_log_events(self):
+        """ Driving method for parsing the log events. Each rule is applied
         to the full log event set. Context events are grouped in a 
         buffer until the first trigger event in their group is seen, 
         and then they are grouped with that instance. """
-    def parse_log_events(self):
+
         for rule in self.event_group_rules:
             search_dict = self.get_event_search_dict(rule)
             context_tags = rule.context_event_tags
