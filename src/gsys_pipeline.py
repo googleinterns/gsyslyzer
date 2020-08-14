d5748247677d91ed7dbd7e16e3d3e1dc21eff19e
rngomez303 <rngomez@google.com>
2020-08-13 19:13:49 -0600

    Added PCIE and SMBus Errors to gsys pipeline. Changed formatting of symptom tags for user-readability

--- src/gsys_pipeline.py
+++ src/gsys_pipeline.py
@@ -26,6 +26,24 @@ if __name__ == "__main__":
             )
     )
 
+    # Add PCIE error event to the builder
+    log_sifter_builder.add_event_rule(
+        event_rule.EventRule(
+            tag="pcie_error",
+            regular_expression="(.*)(Failed read of reg\s)(?P<reg>\d*)(\sfor device at\s)(?P<location>\d*\:\d*\:\d*\.\d*)"
+        )
+    )
+
+    # Add SMBus error event to the builder
+    log_sifter_builder.add_event_rule(
+        event_rule.EventRule(
+            tag="smbus_error",
+            regular_expression="(.*)(SMBus device\s)(?P<dev_num>\d*\-\d*)(:\s)"\
+                               "(?P<failure_type>Read\d*|Write\d*|ReadBlock)"\
+                               "(\sfailure:\s)(?P<msg>.*)"
+        )
+    )
+
     # Add the SIGTERM event to the builder
     log_sifter_builder.add_event_rule(
         event_rule.EventRule(
@@ -58,7 +76,7 @@ if __name__ == "__main__":
         )
     )   
     
-    # Add gsysd version publishing
+    # Add gsysd version publishing event to builder
     log_sifter_builder.add_event_rule(
         event_rule.EventRule(
             tag="gsysd_version_published",
@@ -71,6 +89,24 @@ if __name__ == "__main__":
         event_rule.EventRule(
             tag="gsys_server_start",
             regular_expression="(.*)(initializing gsys server)(.*)"
+        )
+    )
+
+    # Add PCIE error group to the builder
+    log_sifter_builder.add_group_rule(
+        event_group_rule.EventGroupRule(
+            tag="pcie_error",
+            trigger_event_tags=["pcie_error"],
+            context_event_tags=[]
+        )
+    )
+
+    # Add SMBus error group to the builder
+    log_sifter_builder.add_group_rule(
+        event_group_rule.EventGroupRule(
+            tag="smbus_error",
+            trigger_event_tags=["smbus_error"],
+            context_event_tags=[]
         )
     )
 
@@ -110,6 +146,32 @@ if __name__ == "__main__":
         )
     )
 
+    # Add PCIE error criteria to the builder
+    log_sifter_builder.add_criteria(
+        criteria.Criteria(
+            symptom_tag="PCIE Error",
+            signal=signals.ExistenceGroupSignal(
+                tag="pcie_error_signal",
+                group_tag="pcie_error"
+            ),
+            evaluator=evaluators.ExistenceEvaluator(),
+            action_msg="Possible hardare issues exist. Please check the device."
+        )
+    )
+
+    # Add SMBus error criteria to the builder
+    log_sifter_builder.add_criteria(
+        criteria.Criteria(
+            symptom_tag="SMBus Error",
+            signal=signals.ExistenceGroupSignal(
+                tag="smbus_error_signal",
+                group_tag="smbus_error"
+            ),
+            evaluator=evaluators.ExistenceEvaluator(),
+            action_msg="Possible hardare issues exist. Please check the device."
+        )
+    )
+
     # Add rescan loop mode criteria to the builder
     log_sifter_builder.add_criteria(
         criteria.Criteria(
@@ -128,7 +190,7 @@ if __name__ == "__main__":
     # Add Gsys startup criteria to the builder
     log_sifter_builder.add_criteria(
         criteria.Criteria(
-            symptom_tag="Gsys_slow_startup",
+            symptom_tag="Gsys Slow Startup",
             signal=signals.IntervalEventSignal(
                 tag="gsys_startup_signal", 
                 group_tag="gsys_startup", 
